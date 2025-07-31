cdfcd36bfbf58ad51a31268a5f37a3a41a081aea
networkSocerer <gigattfyhig@gmail.com>
2025-07-31 16:01:43 +0900

    hm 스키마

---
+++ model/User.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const userSchema = Schema(
+  {
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    name: { type: String, required: true },
+    level: { type: String, default: "customer" },
+  },
+  { timestamps: true }
+);
+
+userschema.methods.toJSON = function () {
+  const obj = this._doc;
+  delete obj.password;
+  delete obj.__v;
+  delete obj.updatedAt;
+  delete obj.createdAt;
+  return obj;
+};
+
+const User = mongoose.model("User", userSchema);
+module.exports = User;
