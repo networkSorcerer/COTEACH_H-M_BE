33d516848f06220f1a64d8d83ef59be7b9a693cd
networkSocerer <gigattfyhig@gmail.com>
2025-08-08 19:20:38 +0900

    update

--- controller/product.controller.js
+++ controller/product.controller.js
@@ -1,5 +1,5 @@
 const Product = require("../models/Product");
-
+const PAGE_SIZE = 5;
 const productController = {};
 productController.createProduct = async (req, res) => {
   try {
@@ -14,7 +14,7 @@ productController.createProduct = async (req, res) => {
       stock,
       status,
     } = req.body;
-    
+
     let productSku = await Product.findOne({ sku });
     if (productSku) {
       throw new Error("이미 존재하는 식별 코드 입니다.");
@@ -42,16 +42,16 @@ productController.getProducts = async (req, res) => {
     const { page, name } = req.query;
     const cond = name ? { name: { $regex: name, $options: "i" } } : {};
     let query = Product.find(cond);
+    let response = { state: "success" };
+    if (page) {
+      query.skip((page - 1) * 5).limit(PAGE_SIZE);
+      const totalItemNum = await Product.find(cond).count();
+      const totalPageNum = Math.ceil(totalItemNum / PAGE_SIZE);
+      response.totalPageNum = totalPageNum;
+    }
     const productList = await query.exec();
-    // if (name) {
-    //   const products = await Product.find({
-    //     name: { $regex: name, $option: "i" },
-    //   });
-    // } else {
-    //   const products = await Product.find({});
-    // }
-
-    res.status(200).json({ status: "success", productList });
+    response.data = productList;
+    res.status(200).json(response);
   } catch (error) {
     return res.status(400).json({ status: "fail", error: error.message });
   }
